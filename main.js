5539d72e79350740fa3044301987493e7dd319a6
leonardo lobo <loboleonardo75@gmail.com>
2025-01-27 14:05:55 -0400

    Grid

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
-// script.js
+const elemento = document.getElementById("tarjetas");
 
-// Función para cargar el JSON y mostrar los proyectos
-function cargarProyectos() {
+let html = '';
+
+function mostrarProyectos(proyectos) {
+    proyectos.forEach(proyecto => {
+        html = `
+            <div class="proyecto">
+                <img src="${proyecto.imagen}" alt="${proyecto.nombre}" style="width: 400px; height: 400px;">
+                <h1 class="title-project">${proyecto.nombre}</h1>
+                <p class="description-project">${proyecto.descripcion}</p>
+                <a class="button-project" href="${proyecto.url}" target="_black">Github</a>
+            </div>
+        `;
+        elemento.innerHTML += html;
+    });
+}
+
+window.addEventListener('load', () => {
     fetch('proyectos.json')
         .then(response => {
             if (!response.ok) {
@@ -11,30 +26,4 @@ function cargarProyectos() {
         })
         .then(data => mostrarProyectos(data))
         .catch(error => console.log(error));
-}
-
-// Función para mostrar los proyectos en el HTML
-function mostrarProyectos(proyectos) {
-    const elemento = document.getElementById("proyectos");
-    elemento.innerHTML = ''; // Limpiar el contenido previo
-
-    // Crear una variable para almacenar el HTML
-    let html = '<h1>Proyectos Realizados</h1>';
-
-    proyectos.forEach(proyecto => {
-        html += `
-            <div class="proyecto">
-                <img src="${proyecto.imagen}" alt="${proyecto.nombre}" class="Image-project">
-                <h1 class="title-project">${proyecto.nombre}</h1>
-                <p class="description-project">${proyecto.descripcion}</p>
-                <a class="button-project" href="${proyecto.url}" target="_black">Github</a>
-            </div>
-        `;
-    });
-
-    // Asignar el HTML generado a innerHTML una sola vez
-    elemento.innerHTML = html;
-}
-
-// Llamar a la función para cargar los proyectos
-cargarProyectos();
+})
